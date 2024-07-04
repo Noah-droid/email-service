b31f31ba0e3011bd4093d9052d69abda18bc5748
Noah <noahtochukwu10@gmail.com>
2024-07-04 08:25:43 +0100

    ...

--- mail/urls.py
+++ mail/urls.py
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('service/', include('service.urls')),
+    path('', include('service.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
