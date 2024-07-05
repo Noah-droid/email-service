54eaad3a0c8fdd847f5ea00a75dcd581b33fd02c
Noah <noahtochukwu10@gmail.com>
2024-07-06 00:13:31 +0100

    Append Slash

--- service/urls.py
+++ service/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import SendEmail, SendOTP, VerifyOTP
 
 urlpatterns = [
-    path('send-email/', SendEmail.as_view(), name='send_email'),
+    path('send-email', SendEmail.as_view(), name='send_email'),
     path('send-otp/', SendOTP.as_view(), name='send_otp'),
     path('verify-otp/', VerifyOTP.as_view(), name='verify_otp'),
 ]
